3fc6a13caec184cf32ad99981fd2b7954e7a1c5d
Laurent Laville <laurent.laville@gmail.com>
2020-05-25 16:12:21 +0200

    build new fresh distrib version

--- dist/devilbox-cli.sh
+++ dist/devilbox-cli.sh
@@ -608,7 +608,8 @@ get_devilbox_path() {
 main () {
     safe_cd "$(get_devilbox_path)" "Devilbox not found, please make sure it is installed in your home directory or use DEVILBOX_PATH in your profile."
     if [[ $# -eq 0 ]] ; then
-        run_command
+        version_command
+        help_command
     else
         case $1 in
             c|config) shift; config_command "$@";;
